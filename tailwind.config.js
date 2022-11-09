192b516dd55a93f9ccc03926eea6cc924b34f754
Precious Oladele <Oladelep77@gmail.com>
2022-11-09 17:44:13 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'redText': '#DF6951',
+      'purpleText': '#181E4B',
+      'lightPurpleText': '#5E6282',
+      'yellowColor': '#F1A501',
+      'textWhite': '#fff',
+      'buttonColor': '#DF6951',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
